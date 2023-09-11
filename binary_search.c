8a26e7e50d470b15db8e72a495c25dcfde77c4f1
nanidgreat <99958143+nanidgreat@users.noreply.github.com>
2023-09-11 06:01:20 +0000

    binary search recursive and iterative methods

--- binary_search.c
+++ binary_search.c
@@ -12,6 +12,19 @@ int binary_search(int a[], int size, int key)
     }
     return -1;
 }
+int binary_search_recursive(int a[], int low, int high, int key)
+{
+    int mid;
+    if(low > high) return -1;
+    else
+    {
+        mid = (low+high)/2;
+        if (a[mid]== key) return mid;
+        else if (a[mid] < key) binary_search_recursive(a, mid + 1,high,key) ; 
+        else binary_search_recursive(a,low,mid-1,key);
+    }
+    
+}
  int main(void)
 {
     int a[] = {5,8,10,44,67,87,89,90,101};
@@ -19,7 +32,7 @@ int binary_search(int a[], int size, int key)
     int key=0;
     printf("enter key to search\n");
     scanf("%d",&key);
-    int index = binary_search(a,size,key);
+    int index = binary_search_recursive(a,0,size,key);
     if (index >= 0)
     {
         printf("position of the key is %d\n",index);
